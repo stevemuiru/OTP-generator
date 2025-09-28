9913a447cb92dda87458a3c9766f03cc08fc3449
stephen muiru <stevembugua26@gmail.com>
2025-09-28 16:20:10 +0300

    code additions

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const { useState, useEffect, useRef } = React;
 
-function App() {
-  const [count, setCount] = useState(0)
+export const OTPGenerator = () => {
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+const [message, setMessage] = useState('')
+const [count, setCount] = useState(0)
+
+useEffect(() => {
+  setTimeout(() => {
+    
+  })
+}) 
+
+
+const handleClick = () => {
+  setMessage("OTP expired. Click the button to generate a new OTP.")
+  let numbers = [0,1,2,3,4,5,6,7,8,9]
+  
+
 }
 
-export default App
+  return (
+    <div className='container'>
+    <h1 id="otp-title">OTP Generator</h1>
+    <h2 id="otp-display">Click 'Generate OTP' to get a code</h2>
+    <p id="otp-timer" aria-live="polite">{}</p>
+    <button id="generate-otp-button">Generate OTP</button>
+    </div>
+  )
+};

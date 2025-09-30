52e4bc6f74204b27d63d1e187d89b79e6e95bf57
stephen muiru <stevembugua26@gmail.com>
2025-09-30 17:51:55 +0300

    fixing some code

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,35 @@ const { useState, useEffect, useRef } = React;
 export default function OTPGenerator(){
 
 
-const [message, setMessage] = useState('')
+const [otp, setOtp] = useState(null)
 const [count, setCount] = useState(0)
 const [isDisabled, setDisabled] = useState(false)
 
-useEffect(() => {
-  setTimeout(() => {
-   handleClick() 
-  })
-}, [count]) 
-
-
 const handleClick = () => {
-  setMessage("OTP expired. Click the button to generate a new OTP.")
-  let numbers = [0,1,2,3,4,5,6,7,8,9]
-  for(let i = 0; i < numbers.length; i++) {
-    const generatedpassword = Math.floor(Math.random() * numbers)
-  }
-  return generatedpassword
+ const otp = Math.floor(100000 + Math.random() * 900000)
+ setOtp(otp)
 }
+
+useEffect(() => [
+
+],[count])
 
   return (
     <div className='container'>
     <h1 id="otp-title">OTP Generator</h1>
-    <h2 id="otp-display">Click 'Generate OTP' to get a code</h2>
-    <p id="otp-timer" aria-live="polite">{message}</p>
-    <button id="generate-otp-button">Generate OTP</button>
+    <h2 id="otp-display">
+        {otp ? otp : "Click 'Generate OTP' to get a code"}
+      </h2>
+      <p id="otp-timer" aria-live="polite">
+        {count > 0 ? `OTP expires in ${count} seconds` : ""}
+      </p>
+      <button
+        id="generate-otp-button"
+        onClick={handleClick}
+        disabled={isDisabled}
+      >
+        Generate OTP
+      </button>
     </div>
   )
 };
